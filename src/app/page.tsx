32d49c3a15fdeb0997f2a5fb1e275330b6c918e6
Abd-ulrahman <abdo.mohamed15w@gmail.com>
2025-08-02 04:32:57 +0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
+// import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}></main>
+      <main className={styles.main}>hello</main>
     </div>
   );
 }
